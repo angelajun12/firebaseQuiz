b42198117a87cbfdb0bde47700a3d0d7891b964d
Angela (Inhea) Jun <angela.jun12@gmail.com>
2018-06-14 16:16:25 -0400

    Initial Commit

---
+++ firebaseQuiz/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  firebaseQuiz
+//
+//  Created by Angela Jun on 6/14/18.
+//  Copyright © 2018 Angela Jun. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
